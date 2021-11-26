fe5e47828be7196ee82acf528b0dbcd552f5d98c
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-11-26 16:11:01 +0100

    Finished assignment 2 - added form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
@@ -13,6 +14,16 @@ function App() {
         setBananas(0);
         setApples(0);
         setCherries(0);
+    }
+
+    const { register, handleSubmit } = useForm();
+
+    const handleFormSubmit = (data) => {
+        console.log(data);
+        console.log('Aarbeien: ' + strawberries);
+        console.log('Bananen: ' + bananas);
+        console.log('Appels: ' + apples);
+        console.log('Kersen: ' + cherries);
     }
 
   return (
@@ -103,6 +114,98 @@ function App() {
             </button>
         </div>
 
+
+        <form className="form" onSubmit={handleSubmit(handleFormSubmit)}>
+            <fieldset>
+                <legend>Gegevens</legend>
+
+                <label htmlFor="firstname-input">
+                    Voornaam: &nbsp;
+                    <input
+                        type="text"
+                        id="firstname-input"
+                        {...register("firstname")}
+                    />
+                </label>
+
+                <label htmlFor="lastname-input">
+                    Achternaam: &nbsp;
+                    <input
+                        type="text"
+                        id="lastname-input"
+                        {...register("lastname")}
+                    />
+                </label>
+
+                <label htmlFor="age-input">
+                    Leeftijd: &nbsp;
+                    <input
+                        type="number"
+                        id="age-input"
+                        {...register("age")}
+                    />
+                </label>
+
+                <label htmlFor="zipcode-input">
+                    Postcode: &nbsp;
+                    <input
+                        type="text"
+                        id="zipcode-input"
+                        {...register("zipcode")}
+                    />
+                </label>
+
+                <label htmlFor="delivery-input">
+                    Bezorgfrequentie: &nbsp;
+
+                    <select id="delivery-input" {...register("delivery")}>
+                        <option value="daily">Dagelijks</option>
+                        <option value="weekly">Wekelijks</option>
+                        <option value="monthly">Maandelijks</option>
+                    </select>
+                </label>
+
+                <label htmlFor="day" className="radio-button">
+                    <input
+                        type="radio"
+                        id="day"
+                        {...register("day")}
+                    />
+                    Overdag
+                </label>
+
+                <label htmlFor="night" className="radio-button">
+                    <input
+                        type="radio"
+                        id="night"
+                        {...register("night")}
+                    />
+                    's Avonds
+                </label>
+
+                <label htmlFor="comments">
+                    Opmerkingen: &nbsp;
+                </label>
+                <textarea id="comments" cols="30" rows="5" />
+
+                <label htmlFor="terms-and-conditions" className="terms-label">
+                    <input
+                        type="checkbox"
+                        id="terms-and-conditions"
+                    />
+                    Ik ga akkoord met de voorwaarden
+                </label>
+
+                <button
+                    className="submit-btn"
+                    type="submit"
+
+                >
+                    Verzend
+                </button>
+
+            </fieldset>
+        </form>
     </main>
   );
 }
